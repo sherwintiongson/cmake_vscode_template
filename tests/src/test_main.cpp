93e70fc1dd94419567ece8ebfad8d26a46baddcf
tiongsons <sherwin.tiongson@balluff.de>
2025-01-17 16:34:31 +0100

    reorganize project directory

--- tests/src/test_main.cpp
+++ tests/src/test_main.cpp
@@ -1,5 +1,11 @@
 #include <gtest/gtest.h>
 #include <iostream>
+#include <cstdint>
+
+uint32_t add_numbers(uint16_t x, uint16_t y)
+{
+  return x + y;
+}
 
 TEST(SymmetricLutTests, SymmetricLutTests_1)
 {
@@ -13,5 +19,5 @@ TEST(SymmetricLutTests, SymmetricLutTests_2)
 
 TEST(SymmetricLutTests, SymmetricLutTests_3)
 {
-  EXPECT_EQ(7 * 5, 35);
+  EXPECT_EQ(add_numbers(30, 5), 35);
 }
